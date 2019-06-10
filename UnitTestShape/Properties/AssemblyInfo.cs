880839d64d8e5c45b6d0892ecea1e68d1edc389a
Shakhello <51483106+Shakhello@users.noreply.github.com>
2019-06-10 11:42:21 +0300

    Add files via upload

---
+++ UnitTestShape/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestShapeStrategy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Danfoss")]
+[assembly: AssemblyProduct("UnitTestShapeStrategy")]
+[assembly: AssemblyCopyright("Copyright © Danfoss 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1528ef39-306f-4aef-8020-27fff024b269")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestShape/ShapeTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeArea.Model;
+using System;
+
+namespace UnitTestShapeStrategy
+{
+    [TestClass]
+    public class ShapeTest
+    {
+        [TestMethod]
+        public void CircleAreaTest()
+        {
+            var circle = new Circle(3);
+            Assert.AreEqual(Math.PI * 3 * 3, circle.GetArea());
+        }
+        [TestMethod]
+        public void CircleInvalidParamsTest()
+        {
+            var isEx = false;
+            try
+            {
+                var circle = new Circle(-3);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                isEx = true;
+            }
+            Assert.IsTrue(isEx);
+        }
+        [TestMethod]
+        public void TriangleAreaTest()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            Assert.AreEqual(6, triangle.GetArea());
+        }
+        [TestMethod]
+        public void TriangleRightTest()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            Assert.IsTrue(triangle.IsRight());
+        }
+        [TestMethod]
+        public void TriangleInvalidParamsTest()
+        {
+            var isEx = false;
+            try
+            {
+                var circle = new Triangle(-3, 4, 5);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                isEx = true;
+            }
+            Assert.IsTrue(isEx);
+        }
+    }
+}
